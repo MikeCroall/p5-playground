3e8ef93adc4a12c9ffdf93ae1e4dd054e9d22f94
mikecroall <mikebcroall@gmail.com>
2016-12-15 14:25:24 +0000

    Landing page

---
+++ bounce.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>bounce</title>
+		<style>
+			html, body{
+				height: 100%;
+				width: 100%;
+				margin: 0;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		</style>
+	</head>
+	<body>
+		<script src="p5.js"></script>
+		<script src="quicksettings.js"></script>
+		<script src="p5.gui.js"></script>
+		<script src="bounce.js"></script>
+	</body>
+</html>
+
+<!-- NOTE:
+
+Locally, .html works best for browser viewing (unless running apache server)
+
+Heroku, .php is needed for automatic app type detection + deploying
+
+-->
--- index.php
+++ index.php
@@ -1,30 +1,32 @@
-<!DOCTYPE html>
 <html>
-	<head>
-		<title>p5-test</title>
-		<style>
-			html, body{
-				height: 100%;
-				width: 100%;
-				margin: 0;
-				display: flex;
-				justify-content: center;
-				align-items: center;
-			}
-		</style>
-	</head>
-	<body>
-		<script src="p5.min.js"></script>
-		<script src="quicksettings.js"></script>
-		<script src="p5.gui.js"></script>
-		<script src="bounce.js"></script>
-	</body>
+    <head>
+        <title>p5-playground</title>
+        <style>
+            head, body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+            a {
+                font-size: 2em;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+
+            $files = array(
+                'bounce'
+            );
+
+            foreach ($files as &$file) {
+                echo "<a href='$file.php'>$file</a>";
+            }
+
+        ?>
+    </body>
 </html>
-
-<!-- NOTE:
-
-Locally, .html works best for browser viewing (unless running apache server)
-
-Heroku, .php is needed for automatic app type detection + deploying
-
--->
